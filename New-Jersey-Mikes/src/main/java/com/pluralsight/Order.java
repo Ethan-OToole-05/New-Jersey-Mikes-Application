b96ad41c75554806989138e146f33fc2b8579cc5
Ethan.OToole <eotoole005@gmail.com>
2025-05-27 10:00:15 -0400

    order class template made, receipt generator class made as well

--- New-Jersey-Mikes/src/main/java/com/pluralsight/Order.java
+++ New-Jersey-Mikes/src/main/java/com/pluralsight/Order.java
@@ -1,4 +1,51 @@
 package com.pluralsight;
 
+import java.util.List;
+
 public class Order {
+
+    //Holds all of our items that the customer orders.
+    private List<Sandwich> sandwiches;
+    private List<Drink> drinks;
+    private List<Chips> chips;
+    private double totalPrice;
+
+    public Order(List<Sandwich> sandwiches, List<Drink> drinks, List<Chips> chips, double totalPrice) {
+        this.sandwiches = sandwiches;
+        this.drinks = drinks;
+        this.chips = chips;
+        this.totalPrice = totalPrice;
+    }
+
+    public void addSandwich(Sandwich sandwich) {
+        this.sandwiches.add(sandwich);
+    }
+
+    public List<Sandwich> getSandwiches() {
+        return sandwiches;
+    }
+
+    public void addDrink(Drink drink) {
+        this.drinks.add(drink);
+    }
+
+    public List<Drink> getDrinks() {
+        return drinks;
+    }
+
+    public void addChips(Chips chips) {
+        this.chips.add(chips);
+    }
+
+    public List<Chips> getChips() {
+        return chips;
+    }
+
+    public double getTotalPrice() {
+        //TODO: GET THE TOTAL PRICE OF ALL THE IEMS.
+        return 0.0;
+    }
+
+    //TODO: ADD toString SUMMARY FOR ALL ITEMS AND TOTAL PRICE.
+    //
 }
---
+++ New-Jersey-Mikes/src/main/java/com/pluralsight/ReceiptGenerator.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class ReceiptGenerator {
+
+    public static void writeReceipt(Order order) {
+        //get timestamp of current date/time and name it the file name
+        //make the writer
+
+        //loop through all the order items and write each item to the receipt
+
+        //Write the total cost
+
+    }
+
+    public static String makeTimestamp() {
+        //Make time stamp of the current date and time of the order.
+    }
+}
